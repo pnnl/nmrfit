c9224a1ace1e6f0587a5b18246592554f65cfad1
Sean Colby <seanmcolby@gmail.com>
2016-08-23 14:10:48 -0700

    idk

--- interactive_example.py
+++ interactive_example.py
@@ -3,6 +3,10 @@ import matplotlib.pyplot as plt
 import os
 import scipy as sp
 import scipy.signal
+import numpy as np
+
+from scipy.interpolate import UnivariateSpline
+
 
 class AutoPeakSelector:
     def __init__(self, w, u, v):
@@ -19,11 +23,20 @@ class AutoPeakSelector:
         u_peaks = self.u[idx]
         w_peaks = self.w[idx]
 
-        plt.plot(self.w, smoothed)
-        plt.scatter(w_peaks, u_peaks)
-        plt.show()
+        self.w_peaks = w_peaks
+        self.u_peaks = u_peaks
 
         return w_peaks, u_peaks
+
+    def findSigma(self):
+        f = sp.interpolate.interp1d(self.w, self.u)
+
+        for x, y in zip(w_peaks, u_peaks):
+            base = y * 0.1
+            nearest = np.abs(self.u - base).argmin()
+
+
+
 
 
 inDir = "./Data/blindedData/dc_4h_cdcl3_kilimanjaro_25c_1d_1H_2_050616.fid"
@@ -38,5 +51,8 @@ data.select_bounds(low=3.2, high=3.6)
 
 aps = AutoPeakSelector(data.w, data.V, data.I)
 x, y = aps.findMaxima()
+aps.findSigma()
 
-
+# plt.plot(aps.w, aps.u_smoothed)
+# plt.scatter(x, y)
+# plt.show()
