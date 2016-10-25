693730bce3f56862b4378c6d4a8329455b12a40a
Sean Colby <seanmcolby@gmail.com>
2016-10-25 08:33:23 -0700

    minor

--- interactive_example.py
+++ interactive_example.py
@@ -1,15 +1,16 @@
 import NMRfit as nmrft
 import os
+import matplotlib.pyplot as plt
 
 
 # input directory
-inDir = "./Data/blindedData/dc_4b_cdcl3_kilimanjaro_25c_1d_1H_1_043016.fid"
+inDir = "./Data/blindedData/dc_4e_cdcl3_kilimanjaro_25c_1d_1H_1_050116.fid"
 
 # read in data
 data = nmrft.varian_process(os.path.join(inDir, 'fid'), os.path.join(inDir, 'procpar'))
 
 # bound the data
-data.select_bounds()
+data.select_bounds(low=3.23, high=3.6)
 
 
 # select peaks and satellites
@@ -25,20 +26,34 @@ fit = nmrft.FitUtility(data, x0, method='Powell')
 res = fit.generate_result(scale=1)
 
 # summary
-fit.summary()
+# fit.summary()
 
-print(fit.calculate_area_fraction())
+# print(fit.calculate_area_fraction())
 
-# print('\nMoving on to TNC fit:\n')
 
-# # Now we will pass global results onto TNC
-# x0[:3] = res.params[:3]
 
-# # fit data
-# fit = nmrft.FitUtility(data, x0, method='TNC', bounds=bounds, options=None)
+print('\nMoving on to TNC fit:\n')
 
-# # generate result
-# res = fit.generate_result(scale=1)
+# Now we will pass global results onto TNC
+x0[:3] = res.params[:3]
+
+# fit data
+fit = nmrft.FitUtility(data, x0, method='TNC', bounds=bounds, options=None)
+
+# generate result
+res = fit.generate_result(scale=10)
 
 # # summary
 # fit.summary()
+
+plt.plot(fit.data.w, fit.data.V, linewidth=2, alpha=0.5)
+plt.plot(fit.result.w, fit.result.V, linewidth=2, alpha=0.5)
+plt.xlabel('Frequency')
+plt.ylabel('Amplitude')
+plt.show()
+
+plt.plot(fit.data.w, fit.data.I, linewidth=2, alpha=0.5)
+plt.plot(fit.result.w, fit.result.I, linewidth=2, alpha=0.5)
+plt.xlabel('Frequency')
+plt.ylabel('Amplitude')
+plt.show()
