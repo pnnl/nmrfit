a9f4a11d7dfeed673a1e0690486295847ede609b
“Sean <seanmcolby@gmail.com>
2017-02-02 10:21:07 -0800

    case-specific logic

--- benchmark.py
+++ benchmark.py
@@ -20,13 +20,27 @@ l = []
 for exp in experiments:
     ID = os.path.basename(exp)
     sample = ID.split('_')[1]
+    trial = ID.split('_')[-2]
+    end = ID.split('_')[-1].split('.')[0]
     print(ID)
 
     # read in data
     data = nmrft.varian_process(os.path.join(exp, 'fid'), os.path.join(exp, 'procpar'))
 
     # bound the data
-    data.select_bounds(low=3.2, high=3.6)
+    if sample == '4b':
+        if int(trial) in [3, 4, 5]:
+            data.select_bounds(low=3.2, high=3.6)
+        else:
+            data.select_bounds(low=3.25, high=3.6)
+    elif sample == '4d':
+        if end == '072316':
+            data.select_bounds(low=3.23, high=3.6)
+        else:
+            data.select_bounds(low=3.25, high=3.6)
+    else:
+
+        data.select_bounds(low=3.2, high=3.6)
 
     # select peaks and satellites
     peaks = data.select_peaks(method='auto', n=6, plot=False, thresh=0.002)
@@ -40,26 +54,26 @@ for exp in experiments:
     # generate result
     res = fit.generate_result(scale=1)
 
-    row = [ID, sample, 'Powell', res.error, res.area_fraction]
-    if len(row) + len(res.params) == len(c):
-        row.extend(res.params)
-    else:
-        print('Powell fit error...')
-        row.extend(['NA' for i in range(len(c) - len(row))])
-    l.append(row)
+    # row = [ID, sample, 'Powell', res.error, res.area_fraction]
+    # if len(row) + len(res.params) == len(c):
+    #     row.extend(res.params)
+    # else:
+    #     print('Powell fit error...')
+    #     row.extend(['NA' for i in range(len(c) - len(row))])
+    # l.append(row)
 
-    plt.close()
-    plt.plot(fit.data.w, fit.data.V, linewidth=2, alpha=0.5)
-    plt.plot(fit.result.w, fit.result.V, linewidth=2, alpha=0.5)
-    plt.xlabel('Frequency')
-    plt.ylabel('Amplitude')
-    plt.savefig('./results/%s_powell.png' % ID)
+    # plt.close()
+    # plt.plot(fit.data.w, fit.data.V, linewidth=2, alpha=0.5)
+    # plt.plot(fit.result.w, fit.result.V, linewidth=2, alpha=0.5)
+    # plt.xlabel('Frequency')
+    # plt.ylabel('Amplitude')
+    # plt.savefig('./results/%s_powell.png' % ID)
 
     # Now we will pass global results onto TNC
     x0[:3] = res.params[:3]
 
     # fit data
-    fit = nmrft.FitUtility(data, x0, method='TNC', bounds=bounds)
+    fit = nmrft.FitUtility(data, x0, method='TNC', bounds=bounds, options={'maxCGit': 1000, 'maxiter': 1000})
 
     # generate result
     res = fit.generate_result(scale=1)
@@ -78,7 +92,7 @@ for exp in experiments:
     plt.plot(fit.result.w, fit.result.V, linewidth=2, alpha=0.5)
     plt.xlabel('Frequency')
     plt.ylabel('Amplitude')
-    plt.savefig('./results/%s_tnc.png' % ID)
+    plt.savefig('./results/%s_tnc.png' % ID, bbox_inches='tight')
 
 df = pd.DataFrame(l, columns=c)
 df.to_csv('./results/results.csv', index=False)
