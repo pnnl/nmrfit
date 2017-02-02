a7b4bebef8e99205be58e10934746d6ece0c420e
“Sean <seanmcolby@gmail.com>
2017-02-02 12:54:27 -0800

    tweaks for edge cases

--- benchmark.py
+++ benchmark.py
@@ -7,22 +7,18 @@ import warnings
 
 warnings.filterwarnings("ignore")
 
-# input directory
-inDir = "./Data/blindedData/"
-experiments = glob.glob(os.path.join(inDir, '*.fid'))
 
-c = ['id', 'sample', 'method', 'error', 'fraction',
-     'theta', 'GLratio', 'yoff']
-for i in range(1, 7):
-    c.extend(['s' + str(i), 'loc' + str(i), 'a' + str(i)])
-
-l = []
-for exp in experiments:
+def run_fit(exp, method='auto'):
     ID = os.path.basename(exp)
     sample = ID.split('_')[1]
     trial = ID.split('_')[-2]
     end = ID.split('_')[-1].split('.')[0]
     print(ID)
+
+    c = ['id', 'sample', 'method', 'error', 'fraction',
+         'theta', 'GLratio', 'yoff']
+    for i in range(1, 7):
+        c.extend(['s' + str(i), 'loc' + str(i), 'a' + str(i)])
 
     # read in data
     data = nmrft.varian_process(os.path.join(exp, 'fid'), os.path.join(exp, 'procpar'))
@@ -43,7 +39,7 @@ for exp in experiments:
         data.select_bounds(low=3.2, high=3.6)
 
     # select peaks and satellites
-    peaks = data.select_peaks(method='auto', n=6, plot=False, thresh=0.002)
+    peaks = data.select_peaks(method=method, n=6, plot=False, thresh=0.002)
 
     # generate bounds and initial conditions
     x0, bounds = data.generate_initial_conditions()
@@ -54,45 +50,69 @@ for exp in experiments:
     # generate result
     res = fit.generate_result(scale=1)
 
-    # row = [ID, sample, 'Powell', res.error, res.area_fraction]
-    # if len(row) + len(res.params) == len(c):
-    #     row.extend(res.params)
-    # else:
-    #     print('Powell fit error...')
-    #     row.extend(['NA' for i in range(len(c) - len(row))])
-    # l.append(row)
-
-    # plt.close()
-    # plt.plot(fit.data.w, fit.data.V, linewidth=2, alpha=0.5)
-    # plt.plot(fit.result.w, fit.result.V, linewidth=2, alpha=0.5)
-    # plt.xlabel('Frequency')
-    # plt.ylabel('Amplitude')
-    # plt.savefig('./results/%s_powell.png' % ID)
-
-    # Now we will pass global results onto TNC
-    x0[:3] = res.params[:3]
-
-    # fit data
-    fit = nmrft.FitUtility(data, x0, method='TNC', bounds=bounds, options={'maxCGit': 1000, 'maxiter': 1000})
-
-    # generate result
-    res = fit.generate_result(scale=1)
-
-    row = [ID, sample, 'TNC', res.error, res.area_fraction]
+    row = [ID, sample, 'Powell', res.error, res.area_fraction]
     if len(row) + len(res.params) == len(c):
         row.extend(res.params)
     else:
-        print('TNC fit error...')
+        print('Powell fit error...')
         row.extend(['NA' for i in range(len(c) - len(row))])
-
-    l.append(row)
 
     plt.close()
     plt.plot(fit.data.w, fit.data.V, linewidth=2, alpha=0.5)
     plt.plot(fit.result.w, fit.result.V, linewidth=2, alpha=0.5)
     plt.xlabel('Frequency')
     plt.ylabel('Amplitude')
-    plt.savefig('./results/%s_tnc.png' % ID, bbox_inches='tight')
+    plt.savefig('./results/%s_powell.png' % ID)
 
+    # Now we will pass global results onto TNC
+    # x0[1:3] = res.params[1:3]
+
+    # # fit data
+    # fit = nmrft.FitUtility(data, x0, method='TNC', bounds=bounds, options={'maxCGit': 1000, 'maxiter': 1000})
+
+    # # generate result
+    # res = fit.generate_result(scale=1)
+
+    # row = [ID, sample, 'TNC', res.error, res.area_fraction]
+    # if len(row) + len(res.params) == len(c):
+    #     row.extend(res.params)
+    # else:
+    #     print('TNC fit error...')
+    #     row.extend(['NA' for i in range(len(c) - len(row))])
+
+    # plt.close()
+    # plt.plot(fit.data.w, fit.data.V, linewidth=2, alpha=0.5)
+    # plt.plot(fit.result.w, fit.result.V, linewidth=2, alpha=0.5)
+    # plt.xlabel('Frequency')
+    # plt.ylabel('Amplitude')
+    # plt.savefig('./results/%s_tnc.png' % ID, bbox_inches='tight')
+    # plt.close()
+
+    return row
+
+
+auto = pd.read_csv('./results/auto_cases.csv')['id'].values
+manual = pd.read_csv('./results/problematic.csv')['id'].values
+
+# input directory
+inDir = "./Data/blindedData/"
+# experiments = glob.glob(os.path.join(inDir, '*.fid'))
+
+l = []
+# for fn in auto:
+#     exp = os.path.join(inDir, fn)
+#     l.append(run_fit(exp, method='auto'))
+
+for fn in manual:
+    exp = os.path.join(inDir, fn)
+    l.append(run_fit(exp, method='auto'))
+
+# construct column labels
+c = ['id', 'sample', 'method', 'error', 'fraction',
+     'theta', 'GLratio', 'yoff']
+for i in range(1, 7):
+    c.extend(['s' + str(i), 'loc' + str(i), 'a' + str(i)])
+
+# build dataframe
 df = pd.DataFrame(l, columns=c)
 df.to_csv('./results/results.csv', index=False)
